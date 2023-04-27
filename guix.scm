8ce3504d8aa6b23d0f03014d58f6d6dcfe9e9e68
Pierre Neidhardt <mail@ambrevar.xyz>
2023-04-27 18:05:47 +0200

    guix.scm: Add inputs, synopsis, description and license.

--- guix.scm
+++ guix.scm
@@ -29,36 +29,68 @@
 
 (define-public sbcl-prompter
   (package
-   (name "sbcl-prompter")
-   (version "0.0.0")
-   (source
-    (local-file (dirname (current-filename)) #:recursive? #t)
-    ;;;; Or this, in case of contributing to Guix.
-    ;; (origin
-    ;;   (method git-fetch)
-    ;;   (uri (git-reference
-    ;;         (url "https://github.com/atlas-engineer/prompter")
-    ;;         (commit version)))
-    ;;   (file-name (git-file-name "cl-prompter" version))
-    ;;   (sha256
-    ;;    (base32
-    ;;     "SPECIFY-HASH")))
-    )
-   (build-system asdf-build-system/sbcl)
-   ;; We use `cl-*' inputs and not `sbcl-*' ones so that CCL users can also use
-   ;; this Guix manifests.
-   ;;
-   ;; Another reason is to not fail when an input dependency is found in
-   ;; ~/common-lisp, which would trigger a rebuild of the SBCL input in the
-   ;; store, which is read-only and would thus fail.
-   ;;
-   ;; The official Guix package should use `sbcl-*' inputs though.
-   (native-inputs (list cl-lisp-unit2 sbcl))
-   (inputs SPECIFY-INPUTS)
-   (synopsis "ADD A SYNOPSYS.")
-   (home-page "https://github.com/atlas-engineer/prompter")
-   (description "ADD A DESCRIPTION")
-   (license license:specify-license)))
+    (name "sbcl-prompter")
+    (version "0.0.0")
+    (source
+     (local-file (dirname (current-filename)) #:recursive? #t)
+;;;; Or this, in case of contributing to Guix.
+     ;; (origin
+     ;;   (method git-fetch)
+     ;;   (uri (git-reference
+     ;;         (url "https://github.com/atlas-engineer/prompter")
+     ;;         (commit version)))
+     ;;   (file-name (git-file-name "cl-prompter" version))
+     ;;   (sha256
+     ;;    (base32
+     ;;     "SPECIFY-HASH")))
+     )
+    (build-system asdf-build-system/sbcl)
+    ;; We use `cl-*' inputs and not `sbcl-*' ones so that CCL users can also use
+    ;; this Guix manifests.
+    ;;
+    ;; Another reason is to not fail when an input dependency is found in
+    ;; ~/common-lisp, which would trigger a rebuild of the SBCL input in the
+    ;; store, which is read-only and would thus fail.
+    ;;
+    ;; The official Guix package should use `sbcl-*' inputs though.
+    (native-inputs (list cl-lisp-unit2 sbcl))
+    (inputs
+     (list
+      sbcl-alexandria
+      sbcl-calispel
+      sbcl-cl-containers
+      sbcl-cl-str
+      sbcl-closer-mop
+      sbcl-lparallel
+      sbcl-moptilities
+      sbcl-nclasses
+      sbcl-serapeum
+      sbcl-trivial-package-local-nicknames))
+    (synopsis "Live-narrowing, fuzzy-matching, extensible prompt framework")
+    (home-page "https://github.com/atlas-engineer/prompter")
+    (description
+     "This prompter library is heavily inspired by Emacs' minibuffer and
+Helm (@url{https://emacs-helm.github.io/helm/}).  It only deals with the
+backend side of things, it does not handle any display.
+
+Non-exhaustive list of features:
+
+@itemize
+@item Asynchronous suggestion computation.
+@item Multiple sources.
+@item Multiple return actions.
+@item Customizable matching and sorting.
+@item Multiple attributes to match and display (also known as 'multiple column
+display').
+@item Customizable initialization and cleanup functions.
+@item Notifications sent when suggestion list is updated.
+@item Per-source history.
+@item Resumable prompters.
+@item Marks actions (event-driven on marks change).
+@item Current suggestion actions (event-driven on current suggestion change).
+@item Automatically return the prompt when narrowed down to a single suggestion.
+@end itemize\n")
+    (license license:bsd-3)))
 
 (define-public cl-prompter
   (sbcl-package->cl-source-package sbcl-prompter))
